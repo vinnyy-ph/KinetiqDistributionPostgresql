238010b8b5ad3336076f3f466e826c8b9ad562c6
Vincent Ferrer <113322585+vinnyy-ph@users.noreply.github.com>
2025-04-05 17:02:05 +0800

    added external delivery ui

--- kinetiq-frontend/src/modules/Distribution/submodules/InternalDelivery.jsx
+++ kinetiq-frontend/src/modules/Distribution/submodules/InternalDelivery.jsx
@@ -7,7 +7,7 @@ const BodyContent = () => {
             <div className="body-content-container">
                 <p>Hello Internal Delivery SubModule!</p>
                 <p>Fill this container with your elements, change the display if need be.</p>
-                <p>If you're going to style with css, use your unique namespace '.cusrep' at the start.</p>
+                <p>If you're going to style with css, use your unique namespace '.internal-delivery' at the start.</p>
             </div>
         </div>
 
